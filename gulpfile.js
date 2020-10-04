c7d3f815323428543e17a24dafcb72638f62af12
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-10-04 18:48:57 +0300

    Publish

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+/**
+ * SASS-KIT for SASS development.
+ * 
+ * 
+ * 
+ * @package     sass-kit
+ * @author      ÖZTAŞ, Emre Can <me@emrecanoztas.com>
+ * @copyright   2020
+ * @license     MIT
+ * @link        https://github.com/ecoztas/sass-kit
+ * @since       v1.0
+ */
+
+// DEFINE: Package(s).
+// =============================================================================
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+
+// TASK: Default
+// =============================================================================
+gulp.task('default', function() {
+    console.log('Works fine!');
+});
+
+// TASK: SASS
+// =============================================================================
+gulp.task('sass', function() {
+    return (
+        gulp.src('public/template/sass-kit/theme.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('public/styles'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('public/styles'))
+    );
+});
---
+++ public/scripts/theme.js
@@ -0,0 +1,1 @@
+// Your JavaScript codes here.
